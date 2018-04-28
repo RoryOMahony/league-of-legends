b3bf80458cee7f0545e81da8fce3618bdd5454bb
Rory O'Mahony <rory1510@hotmail.co.uk>
2018-04-28 19:22:08 +0100

    Created a basic navigation bar.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import NavigationBar from './navigationbar/NavigationBar';
 import ChampionNames from './championnames/ChampionNames';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<NavigationBar />, document.getElementById('navigationBar'));
 ReactDOM.render(<ChampionNames />, document.getElementById('championNames'));
